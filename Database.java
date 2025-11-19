804c5c03a94472dee3ee4192ec25ddaeae9121ac
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2025-11-19 17:29:58 +0100

    Update Database.java

--- Database.java
+++ Database.java
@@ -13,6 +13,7 @@ public class Database{
     private Scanner dbReader;
     private final Random random = new Random();
 
+    // 👍
     public Database(String dbPath) {
         this.dataBasePath = dbPath;
         main.log.appendInfo("Database path setted to " + this.getDataBasePath() + ".");
@@ -20,6 +21,7 @@ public class Database{
         loadDatabase(dataBasePath);
     }
 
+    // 👍
     // Database loader
     public void loadDatabase(String name) {
         main.log.appendInfo("Loading database.");
@@ -92,6 +94,7 @@ public class Database{
         main.log.appendInfo("Loaded database successfully.");
     }
 
+    // 👍
     // Create database method
     public void createDatabase(String name) {
         try {
@@ -108,20 +111,24 @@ public class Database{
                 // It tries to make another one with the original name + the random number
                 createDatabase(name + random.nextInt(0,10000));
             }
-            // Catches an error if happens
+
         } catch (Exception err) {
             // Print the error in the log
             main.log.appendErr("An error occurred the session: " + err.getMessage() + "\n Cause: " + err.getCause());
         }
     }
 
+    // 👍
     // Get element method
     public String getElement(String elementName) {
         // Create a string
         String currentLine = "";
         try {
             // Re initialize scanner
+            main.log.appendInfo("Initializing scanner.");
             this.dbReader = new Scanner(jsonDB);
+            main.log.appendInfo("Scanner initialized successfully.");
+
             // Loop until end  of file (EOF)
             while (dbReader.hasNext()) {
                 // Read line
@@ -138,9 +145,10 @@ public class Database{
                 }
             }
             // Close the streams
+            main.log.appendInfo("Closing scanner.");
             dbReader.close();
+            main.log.appendInfo("Scanner closed successfully.");
 
-            // Catch an error if happens
         } catch (Exception err) {
             // Print the error in the log
             main.log.appendErr("An error occurred the session: " + err.getMessage() + "\n Cause: " + err.getCause());
@@ -148,20 +156,54 @@ public class Database{
         return currentLine;
     }
 
-    // Get parameters method
-    public HashMap<String, String> getParameters(String elementName) {
-        // Creates a string
-        String currentLine;
-        // Creates an HashMap obj
-        HashMap<String, String> parameters = new HashMap<>();
+    // TO FINISH
+    public String[] getElements() {
+        // Create a string
+        String currentLine = "";
         try {
             // Re initialize scanner
             this.dbReader = new Scanner(jsonDB);
+            // Loop until end  of file (EOF)
             while (dbReader.hasNext()) {
+                // Read line
+                currentLine = dbReader.nextLine();
+
+            }
+            // Close the streams
+            main.log.appendInfo("Closing scanner.");
+            dbReader.close();
+            main.log.appendInfo("Scanner closed successfully.");
+
+        } catch (Exception err) {
+            // Print the error in the log
+            main.log.appendErr("An error occurred the session: " + err.getMessage() + "\n Cause: " + err.getCause());
+        }
+        return null;
+    }
+
+    // 👍
+    // Get parameters method
+    public HashMap<String, String> getAttributes(String elementName) {
+        // Creates a string
+        String currentLine;
+
+        // Creates an HashMap obj
+        HashMap<String, String> parameters = new HashMap<>();
+        try {
+
+            // Re initialize scanner
+            main.log.appendInfo("Initializing scanner.");
+            this.dbReader = new Scanner(jsonDB);
+            main.log.appendInfo("Scanner initialized successfully.");
+
+            while (dbReader.hasNext()) {
+
                 // Read line in the file
                 currentLine = dbReader.nextLine();
+
                 // If contains the element name
                 if (currentLine.contains(elementName)) {
+
                     // Remove all non-important chars
                     currentLine = currentLine
                             .replace(" ", "")
@@ -172,12 +214,16 @@ public class Database{
 
                     // Creates an array by splitting the line
                     String[] pairs = currentLine.split(",");
+
                     for (String pair : pairs) {
+
                         // Creates an array by splitting each element of the array
                         String[] keyValue = pair.split(":", 2);
+
                         if (keyValue.length == 2) {
                             String key = keyValue[0];
                             String value = keyValue[1];
+
                             // Add the parameters to the hashmap
                             parameters.put(key, value);
                         }
@@ -186,45 +232,68 @@ public class Database{
                 }
             }
             // Close the streams
+            main.log.appendInfo("Closing scanner.");
             dbReader.close();
-            // Catch an error if happens
+            main.log.appendInfo("Scanner closed successfully.");
+
         } catch (Exception err) {
             // Print the error in the log
             main.log.appendErr("An error occurred the session: " + err.getMessage() + "\n Cause: " + err.getCause());
         }
         return parameters;
     }
+
+
+    // 👍
+    // add element method
     public void addElement(String name) {
         // Check if the element is already present
         if (getElement(name)!=null) { // TO FIX
             main.log.appendWarn("Element already in database. Skipping method");
+
         } else {
+
             try {
                 // Creates a StringBuilder object to store the lines of the file
                 StringBuilder dbContent = new StringBuilder();
+
                 // Re-initialize scanner
+                main.log.appendInfo("Initializing scanner.");
                 this.dbReader = new Scanner(jsonDB);
+                main.log.appendInfo("Scanner initialized successfully.");
+
                 // Start reading the file until EOF (end of file)
                 while (dbReader.hasNextLine()) {
+
                     String line = dbReader.nextLine();
                     if (line.equals("}")) {
                         break;
                     }
+
                     // Every line is added to the string builder "dbContent"
                     dbContent.append(line).append("\n");
                 }
                 // Close the stream
+                main.log.appendInfo("Closing scanner.");
                 dbReader.close();
+                main.log.appendInfo("Scanner closed successfully.");
+
                 // Re-initialize the writer
+                main.log.appendInfo("Initializing FileWriter.");
                 this.dbWriter = new FileWriter(jsonDB);
+                main.log.appendInfo("FileWriter initialized successfully.");
+
                 // Write the content
                 dbWriter.write(dbContent.toString());
                 dbWriter.write("\t\"" + name + "\"");
                 // Write the end of the file with "}"
                 dbWriter.write("\n}");
+
                 // Close the stream
+                main.log.appendInfo("Closing FileWriter.");
                 dbWriter.close();
-                // Catches an error if happens
+                main.log.appendInfo("FileWriter closed successfully.");
+
             } catch (Exception err) {
                 // Print the error in the log
                 main.log.appendErr("An error occurred in the session: " + err.getMessage() + "\n Cause: " + err.getCause());
