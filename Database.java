7c556a25d588004f46ade99d7472b9fe7c472de0
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2025-11-22 15:04:50 +0100

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,131 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Scanner;
+
+@SuppressWarnings("unused")
+class Database {
+    // Database file variable
+    private File db;
+    public File getDatabase() {return db;}
+    public void setDatabase(File file) {
+        db = file;
+    }
+    // Database writer variable & methods
+    private FileWriter dbWriter;
+    private void loadWriter() throws IOException{
+        dbWriter = new FileWriter(db);
+    }
+    private void unloadWriter() throws IOException {
+        dbWriter.close();
+    }
+    // Database reader variable & methods
+    private Scanner dbReader;
+    private void loadScanner() throws FileNotFoundException{
+        dbReader = new Scanner(db);
+    }
+    private void unloadScanner() throws FileNotFoundException {
+        dbReader.close();
+    }
+    // String builder
+    private final StringBuilder content = new StringBuilder();
+    private void clearContent() {
+        if (!content.isEmpty()) {
+            content.delete(0, content.length());
+        } else {
+            return;
+        }
+
+    }
+
+    // Constructors
+    public Database() {}
+    public Database(File file) {
+        setDatabase(file);
+    }
+
+    // Create database method
+    public void createDatabase(String name) throws IOException {
+        File file = new File(name);
+        if (file.createNewFile()) {
+            setDatabase(file);
+        } else {
+            throw new FileAlreadyExistsException(file.getName());
+        }
+    }
+    public void createDatabase() throws IOException {
+        if (db.createNewFile()) {
+            setDatabase(db);
+        } else {
+            throw new FileAlreadyExistsException(db.getName());
+        }
+    }
+
+    public StringBuilder readDatabase(String name) throws IOException {
+        File file = new File(name);
+        StringBuilder tempStrBuildr;
+        if (file.exists()) {
+            loadScanner();
+            while (dbReader.hasNextLine()) {
+                content.append(dbReader.nextLine()).append("\n");
+            }
+            tempStrBuildr = new StringBuilder(content.toString());
+            clearContent();
+        } else {
+            throw new FileNotFoundException();
+        }
+        return tempStrBuildr;
+    }
+    public StringBuilder readDatabase() throws IOException {
+        StringBuilder tempStrBuildr;
+        if (db.exists()) {
+            loadScanner();
+            while (dbReader.hasNextLine()) {
+                content.append(dbReader.nextLine()).append("\n");
+            }
+            tempStrBuildr = new StringBuilder(content.toString());
+            clearContent();
+        } else {
+            throw new FileNotFoundException();
+        }
+        return tempStrBuildr;
+    }
+
+    /*
+    // Load database methods
+    public void loadDatabase(String name) throws IOException {
+        File file = new File(name);
+        if (file.exists()) {
+            loadScanner();
+            while (dbReader.hasNextLine()) {
+                content.append(dbReader.nextLine()).append("\n");
+            }
+            unloadScanner();
+            loadWriter();
+            dbWriter.write(content.toString());
+            unloadWriter();
+            clearContent();
+        } else {
+            throw new FileNotFoundException();
+        }
+    }
+    public void loadDatabase() throws IOException {
+        if (db.exists()) {
+            loadScanner();
+            while (dbReader.hasNextLine()) {
+                content.append(dbReader.nextLine()).append("\n");
+            }
+            unloadScanner();
+            loadWriter();
+            dbWriter.write(content.toString());
+            unloadWriter();
+            clearContent();
+        } else {
+            throw new FileNotFoundException();
+        }
+    }*/
+
+
+}
